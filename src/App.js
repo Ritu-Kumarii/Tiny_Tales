e406dfe7891c55e5d0b65948ee8c8f1696bd2138
Ritu Kumari <ritukumari@GVVW2QM66M.local>
2024-02-19 14:59:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import EditCreatePost from "./pages/EditCreatePost";
+import PostDetails from "./pages/PostDetails";
+import Navbar from "./components/Navbar";
+import "./styles/base.css";
+import AuthorPage from "./pages/AuthorPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/author/:author" element={<AuthorPage />} />
+          <Route path="/post/:postId" element={<PostDetails />} />
+          <Route path="/post/:postId/edit" element={<EditCreatePost />} />
+          <Route path="/post/create" element={<EditCreatePost />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
